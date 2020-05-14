264eb7caf2240f7ee98040b0632dfa3487761b7b
ElyorUralov <krasavchik_number_one@mail.ru>
2020-05-15 03:41:36 +0500

    Урок 8 - обновление

---
+++ src/main/java/com/jeshco/backendspringboot/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package com.jeshco.backendspringboot.controller;
+
+import com.jeshco.backendspringboot.entity.Category;
+import com.jeshco.backendspringboot.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category/")
+public class CategoryController {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("test")
+    public List<Category> test() {
+        List<Category> list = categoryRepository.findAll();
+        return list;
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<Category> add(@RequestBody Category category) {
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("update")
+    public ResponseEntity update(@RequestBody Category category) {
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+}
