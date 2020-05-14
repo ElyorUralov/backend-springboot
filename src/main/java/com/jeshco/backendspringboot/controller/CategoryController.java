f6cad349cce32da47d6521d1f5e81d73aa065388
ElyorUralov <krasavchik_number_one@mail.ru>
2020-05-15 04:01:45 +0500

    Урок 9 - Удаление

--- src/main/java/com/jeshco/backendspringboot/controller/CategoryController.java
+++ src/main/java/com/jeshco/backendspringboot/controller/CategoryController.java
@@ -2,11 +2,13 @@ package com.jeshco.backendspringboot.controller;
 
 import com.jeshco.backendspringboot.entity.Category;
 import com.jeshco.backendspringboot.repository.CategoryRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/category/")
@@ -48,6 +50,30 @@ public class CategoryController {
         }
 
         return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @GetMapping("id/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = null;
+        try {
+            category = categoryRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(category);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
--- src/main/java/com/jeshco/backendspringboot/controller/PriorityController.java
+++ src/main/java/com/jeshco/backendspringboot/controller/PriorityController.java
@@ -3,11 +3,13 @@ package com.jeshco.backendspringboot.controller;
 import com.jeshco.backendspringboot.entity.Priority;
 import com.jeshco.backendspringboot.repository.PriorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/priority/")
@@ -43,7 +45,7 @@ public class PriorityController {
         return ResponseEntity.ok(priorityRepository.save(priority));
     }
 
-    @PutMapping
+    @PutMapping("update")
     public ResponseEntity update(@RequestBody Priority priority) {
         if (priority.getId() == null || priority.getId() == 0) {
             return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
@@ -56,8 +58,31 @@ public class PriorityController {
         if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
             return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
         }
-
         return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @GetMapping("id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id) {
+        Priority priority = null;
+        try {
+            priority = priorityRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priority);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        try {
+            priorityRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 
